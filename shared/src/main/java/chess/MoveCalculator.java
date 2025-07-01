93fd020c6334199da82b628023a5206d0dd05e42
Brandon Monson <124306942+SauzyOmp@users.noreply.github.com>
2025-06-30 21:08:58 -0600

    basis of move calculator

---
+++ shared/src/main/java/chess/MoveCalculator.java
@@ -0,0 +1,55 @@
+package chess;
+
+import java.util.Collection;
+
+public class MoveCalculator {
+    public static Collection<ChessMove> generateMoves(ChessPiece piece, ChessPosition position, ChessBoard board) {
+        switch (piece.getPieceType()) {
+            case KING:
+                return calcKingMoves(position, board);
+
+            case QUEEN:
+                return calcQueenMoves(position, board);
+            
+            case BISHOP:
+                return calcBishopMoves(position, board);
+            
+            case KNIGHT:
+                return calcKnightMoves(position, board);
+
+            case ROOK:
+                return calcRookMoves(position, board);
+
+            case PAWN:
+                return calcPawnMoves(position, board);
+        
+            default:
+                break;
+        }
+    }
+
+    private static Collection<ChessMove> calcKingMoves( ChessPosition position, ChessBoard board) {
+        
+    }
+
+    private static Collection<ChessMove> calcQueenMoves( ChessPosition position, ChessBoard board) {
+        
+    }
+
+    private static Collection<ChessMove> calcBishopMoves( ChessPosition position, ChessBoard board) {
+        
+    }
+
+    private static Collection<ChessMove> calcKnightMoves( ChessPosition position, ChessBoard board) {
+        
+    }
+
+    private static Collection<ChessMove> calcRookMoves( ChessPosition position, ChessBoard board) {
+        
+    }
+
+    private static Collection<ChessMove> calcPawnMoves( ChessPosition position, ChessBoard board) {
+        
+    }
+
+}
