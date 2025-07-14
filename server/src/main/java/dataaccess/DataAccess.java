0c730855a93e510a0b19f717746ef06aa1f2703c
Brandon Monson <124306942+SauzyOmp@users.noreply.github.com>
2025-07-13 23:58:05 -0600

    initial nethods

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,21 @@
+package dataaccess;
+
+import Model.AuthData;
+import Model.GameData;
+import Model.UserData;
+import org.eclipse.jetty.server.Authentication;
+
+import java.util.List;
+
+public interface DataAccess {
+    void clear() throws DataAccessException;
+    void createUser(String username, String password) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+    GameData createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    List<GameData> listGames() throws DataAccessException;
+    void updateGame(int gameID) throws DataAccessException;
+    AuthData createAuth(String username) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+}
