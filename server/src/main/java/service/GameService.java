d795b47e03d32a9a5faf6c92208a74d6f97b9e54
Brandon Monson <124306942+SauzyOmp@users.noreply.github.com>
2025-07-15 21:51:54 -0600

    Gameservice done?

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,65 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import Model.AuthData;
+import Model.GameData;
+
+import java.util.List;
+
+public class GameService {
+    private final DataAccess dao;
+
+    public GameService(DataAccess dao) {
+        this.dao = dao;
+    }
+
+    public void clear() throws DataAccessException {
+        dao.clear();
+    }
+
+    public GamesResult listGames(String authToken) throws DataAccessException {
+        dao.getAuth(authToken);
+        List<GameData> all = dao.listGames();
+        return new GamesResult(all);
+    }
+
+    public CreateGameResult createGame(String authToken,
+                                       CreateGameRequest req)
+            throws DataAccessException {
+        dao.getAuth(authToken);
+        GameData created = dao.createGame(req.gameName());
+        return new CreateGameResult(created.gameID());
+    }
+
+    public void joinGame(String authToken,
+                         JoinGameRequest req)
+            throws DataAccessException {
+        AuthData auth = dao.getAuth(authToken);
+
+        GameData existing = dao.getGame(req.gameID());
+
+        String color = req.playerColor();
+        boolean whiteTaken = existing.whiteUsername() != null;
+        boolean blackTaken = existing.blackUsername() != null;
+        if (("WHITE".equals(color) && whiteTaken) ||
+                ("BLACK".equals(color) && blackTaken)) {
+            throw new DataAccessException("game full");
+        }
+
+        String white = existing.whiteUsername();
+        String black = existing.blackUsername();
+        if ("WHITE".equals(color)) white = auth.username();
+        else                          black = auth.username();
+
+        GameData updated = new GameData(
+                existing.gameID(),
+                white,
+                black,
+                existing.gameName(),
+                existing.game()
+        );
+
+        dao.updateGame(updated);
+    }
+}
---
+++ server/src/main/java/service/GamesResult.java
@@ -0,0 +1,8 @@
+package service;
+
+import Model.GameData;
+
+import java.util.List;
+
+public record GamesResult(List<GameData> games) {
+}
