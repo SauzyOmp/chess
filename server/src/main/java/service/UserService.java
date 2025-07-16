917cca3d7dc25a99e2dabb6d115501db8c86ffdf
Brandon Monson <124306942+SauzyOmp@users.noreply.github.com>
2025-07-15 21:24:33 -0600

    Userservice done?

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,48 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import Model.UserData;
+import Model.AuthData;
+
+public class UserService {
+    private final DataAccess dao;
+
+    public UserService(DataAccess dao) {
+        this.dao = dao;
+    }
+
+    public RegisterResult register(RegisterRequest req) throws DataAccessException {
+        try {
+            dao.getUser(req.username());
+            throw new DataAccessException("username already taken");
+        } catch (DataAccessException e) {
+            if (!e.getMessage().contains("not found")) throw e;
+        }
+
+        UserData newUser = new UserData(req.username(), req.password(), req.email());
+        dao.createUser(newUser);
+        AuthData auth = dao.createAuth(req.username());
+        return new RegisterResult(auth.username(), auth.authToken());
+    }
+
+    public LoginResult login(LoginRequest req) throws DataAccessException {
+        if (req.username() == null || req.password() == null) {
+            throw new DataAccessException("bad request");
+        }
+
+        UserData stored = dao.getUser(req.username());
+
+        if (!stored.password().equals(req.password())) {
+            throw new DataAccessException("username/password incorrect");
+        }
+
+        AuthData auth = dao.createAuth(req.username());
+        return new LoginResult(auth.username(), auth.authToken());
+    }
+
+    public void logout(String authToken) throws DataAccessException {
+        dao.getAuth(authToken);
+        dao.deleteAuth(authToken);
+    }
+}
