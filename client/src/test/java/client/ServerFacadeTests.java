5051e3d79a3333c9d9620ff3a10b3133e8303f46
Brandon Monson <124306942+SauzyOmp@users.noreply.github.com>
2025-07-27 18:35:33 -0600

    login tests pass

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -2,6 +2,7 @@ package client;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import service.LoginResult;
 import service.RegisterResult;
 import client.ServerFacade;
 import exception.ResponseException;
@@ -42,6 +43,21 @@ public class ServerFacadeTests {
     public void registerNegative() throws Exception {
         facade.register("user2", "pass2", "reggie2@example.com");
         Assertions.assertThrows(ResponseException.class, () -> facade.register("user2", "passX", "reggie2b@example.com"));
+    }
+
+    @Test
+    public void loginPositive() throws Exception {
+        facade.register("user3", "pass3", "reggie3@example.com");
+        LoginResult result = facade.login("user3", "pass3");
+        Assertions.assertNotNull(result.authToken());
+        Assertions.assertFalse(result.authToken().isEmpty());
+        Assertions.assertEquals("user3", result.username());
+    }
+
+    @Test
+    public void loginNegative() throws Exception {
+        facade.register("user4", "pass4", "reggie4@example.com");
+        Assertions.assertThrows(ResponseException.class, () -> facade.login("user4", "wrong"));
     }
 
 }
